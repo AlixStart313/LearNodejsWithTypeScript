785e2453015451559b8101801d51cfcd2c313978
Lisseth Fuentes <105747977+AlixStart313@users.noreply.github.com>
2023-05-11 23:04:12 -0600

    Update usersController.ts

--- controllers/usersController.ts
+++ controllers/usersController.ts
@@ -16,4 +16,30 @@ export const getUser= (req:Request,res:Response)=>{
 }
 
 
-export const postUser
+export const postUser  = (req: Request,res : Response)=>{
+    const {body} = req;
+
+    res.json({
+        msg:'postUser',
+        body
+    })
+}
+
+export const putUser  = (req: Request,res : Response)=>{
+    const {id} = req.params;
+    const {body} = req;
+
+    res.json({
+        msg:'putUser',
+        body
+    })
+}
+
+export const deleteUser  = (req: Request,res : Response)=>{
+    const {id} = req.params;
+
+    res.json({
+        msg:'deleteUser',
+        id
+    })
+}
