56d3576d7a39cbb505d65a1a9805bd85eb3d3802
Lisseth Fuentes <105747977+AlixStart313@users.noreply.github.com>
2023-05-11 23:03:51 -0600

    Update server.ts

--- models/server.ts
+++ models/server.ts
@@ -1,14 +1,28 @@
 import express,{Application} from "express";
+import userRouter from "../routes/usersRoutes";
+
+//import * as userRoutes from "../routes/usersRoutes";
+//exporta todo el paquete y sera almacenado con ese nombre
 
 
 class Server{
 
     private app: Application;
     private port: string;
+    private apiPaths ={
+        usuarios: '/api/users'
+
+    };
 
     constructor(){
         this.app=express();
         this.port=process.env.PORT || '8000';
+        //definimos las rutas
+        this.routes();
+    }
+
+    routes(){
+        this.app.use(this.apiPaths.usuarios, userRouter)
     }
 
     listen(){
